172da2e0f12bd956ff6e8a59d357a659f42e538a
kstrauch94 <kstrauch0694@gmail.com>
2018-12-20 01:21:53 +0100

    add test_config with some explanations of options

---
+++ test_config.py
@@ -0,0 +1,16 @@
+MINIMAL = "minimal" # Minize nogoods, requires validation
+
+SORTBY = "sortby" # list of properties to sort the nogoods in order of sorting priority
+                  # possible values: degree, literal_count, ordering, lbd
+
+REVERSE_SORT = "reverse_sort" # sort in reverse order
+
+VALIDATE_FILES = "validate_files" # if file is given it will validate using this file
+                                  # use None to disable validation
+
+CONFIG = {  MINIMAL : False,
+            SORTBY : ["degree", "literal_count"],
+            REVERSE_SORT : False,
+            #VALIDATE_FILES : ["state_prover.lp"]
+            VALIDATE_FILES : ["state_prover-new-rules.lp"]
+}
