1135d2db1cabf00aacaa70e31a4e8d4b26613647
kstrauch94 <kstrauch0694@gmail.com>
2019-01-14 13:41:49 +0100

    deleted some options that are now passed via command line options

--- test_config.py
+++ test_config.py
@@ -5,12 +5,7 @@ SORTBY = "sortby" # list of properties to sort the nogoods in order of sorting p
 
 REVERSE_SORT = "reverse_sort" # sort in reverse order
 
-VALIDATE_FILES = "validate_files" # if file is given it will validate using this file
-                                  # use None to disable validation
-
 CONFIG = {  MINIMAL : False,
             SORTBY : ["degree", "literal_count"],
             REVERSE_SORT : False,
-            VALIDATE_FILES : ["validation-encoding/state_prover.lp"]
-            #VALIDATE_FILES : ["state_prover-new-rules.lp"]
 }
