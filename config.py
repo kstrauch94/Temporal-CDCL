c4120d1913b440325e95dcff8c687424142aa283
kstrauch94 <kstrauch0694@gmail.com>
2019-06-07 13:32:42 +0200

    add helper function to config

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 import os
 
+def get_parent_dir(path):
+    # this gets the name of the parent folder
+
+    # if there is a trailing backslash then delete it
+    if path.endswith("/"):
+        path = path[:-1]
+
+    return os.path.dirname(path)
+
 FD_CALL = ["/home/klaus/bin/Fast-Downward/fast-downward.py", "--translate"]
 
 FILE_PATH = os.path.abspath(__file__)
