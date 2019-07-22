1f5b8994664839ff74c09b548603819b1ade9f11
kstrauch94 <kstrauch0694@gmail.com>
2019-07-22 13:22:17 +0200

    update the config file and add some comments

--- config_file.example.py
+++ config_file.example.py
@@ -1,18 +1,13 @@
 import os
+from tools.tools import get_parent_dir
 
-def get_parent_dir(path):
-    # this gets the name of the parent folder
-
-    # if there is a trailing backslash then delete it
-    if path.endswith("/"):
-        path = path[:-1]
-
-    return os.path.dirname(path)
-
-FD_CALL = ["/home/klaus/bin/Fast-Downward/fast-downward.py", "--translate"]
+# Path to the fast downward python call
+FD_CALL = ["/Path/to/Fast-Downward/fast-downward.py", "--translate"]
 
 FILE_PATH = os.path.abspath(__file__)
 
+# Path to the runsolver binary (this works for a binary called "runsolver" that is the same directory)
 RUNSOLVER_PATH = os.path.join(get_parent_dir(FILE_PATH), "runsolver") 
 
+# Path to plasp call
 PLASP = "plasp"
