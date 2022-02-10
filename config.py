84a073f465d027394061ed54b41a9dda34d6d763
kstrauch94 <kstrauch0694@gmail.com>
2022-02-10 13:18:10 +0100

    rename

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ FD_CALL = ["/Path/to/Fast-Downward/fast-downward.py", "--translate"]
 FILE_PATH = os.path.abspath(__file__)
 
 # Path to the runsolver binary (this works for a binary called "runsolver" that is the same directory)
-RUNSOLVER_PATH = os.path.join(get_parent_dir(FILE_PATH), "runsolver") 
+RUNSOLVER_PATH = os.path.abspath(os.path.join(get_parent_dir(FILE_PATH), "runsolver"))
 
 # Path to plasp call
 PLASP = "plasp"
