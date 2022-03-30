d4fb88f6241faa8b20e79ac4d35ba88bc21fc725
kstrauch <kstrauch0694@gmail.com>
2022-03-30 13:20:37 +0200

    add more options and use deg 10 as default

--- inc_options/size50-deg8-lbd3.py
+++ inc_options/size50-deg8-lbd3.py
@@ -1,7 +1,7 @@
 #script (python) 
 
 OPTIONS = {"max_size": 50,
-            "max_degree": 8,
+            "max_degree": 10,
             "max_lbd": 3}
 
 #end.
--- inc_options/size50-deg8-max-500.py
+++ inc_options/size50-deg8-max-500.py
@@ -1,6 +1,7 @@
 #script (python) 
 
 OPTIONS = {"max_size": 50,
-            "max_degree": 10
+            "max_degree": 10,
+            "max_nogoods": 500
             }
 #end.
