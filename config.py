a097e53fca934dee8561125fec5a30c2aa41f37d
kstrauch94 <kstrauch0694@gmail.com>
2022-05-20 12:44:00 +0200

    delete old options

--- config.py
+++ config.py
@@ -2,14 +2,9 @@ import os
 from tools.tools import get_parent_dir
 
 # Path to the fast downward python call
-FD_CALL = ["/Path/to/Fast-Downward/fast-downward.py", "--translate"]
-
 FILE_PATH = os.path.abspath(__file__)
 
 # Path to the runsolver binary (this works for a binary called "runsolver" that is the same directory)
 RUNSOLVER_PATH = os.path.abspath(os.path.join(get_parent_dir(FILE_PATH), "runlim"))
-
-# Path to plasp call
-PLASP = "plasp"
 
 CLINGO = "clingo"
