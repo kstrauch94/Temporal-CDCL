f2f5a8dfd9c316abe5d520187bb3a09c61c737e2
kstrauch94 <kstrauch0694@gmail.com>
2023-05-02 12:49:29 +0200

    delete old files

--- inc_options/base.py
+++
@@ -1,7 +0,0 @@
-#script (python) 
-
-OPTIONS = { "max_size": 50,
-            "max_degree": 10,
-            "base_benchmark_mode": True
-            }
-#end.
--- inc_options/size50-deg10-max500.py
+++
@@ -1,7 +0,0 @@
-#script (python) 
-
-OPTIONS = {"max_size": 50,
-            "max_degree": 10,
-            "max_nogoods": 500
-            }
-#end.
